4f817c9ba984be54243321dc2efcbf5b17e47f4a
Hatchet <hatchet.codes@gmail.com>
2018-01-17 18:20:09 -0600

    Refactor to stateless components

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const list = [
   },
 ];
 
-const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   constructor(props) {
@@ -56,7 +55,9 @@ class App extends Component {
         <Search
           value={searchTerm}
           onChange={this.onSearchChange}
-        />
+        >
+          Search
+         </Search>
         <Table
           list={list}
           pattern={searchTerm}
@@ -67,43 +68,41 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange } = this.props;
-    return (
-      <form>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange} />
-      </form>
-    )
-  }
-}
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children}
+    <input
+      type="text"
+      value={value}
+      onChange={onChange} />
+  </form>
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <button
-              type="button"
-              onClick={() => onDismiss(item.objectID)}
-            >
-              Dismiss
-            </button>
-          </div>
-        )}
+
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <Button
+          onClick={() => onDismiss(item.objectID)}
+        >
+          Dismiss
+            </Button>
       </div>
-    );
-  }
-}
+    )}
+  </div>
+
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    type="button"
+    onClick={onClick}
+    class={className}
+  > {children}
+  </button>
 export default App;
